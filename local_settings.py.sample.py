fc90c43af25fa295883eb02ee1c0594d497ebded
Alex Button <abutton@gmail.com>
2014-01-27 23:52:46 -0800

    added local_settings sample for configuration

---
+++ local_settings.py.sample.py
@@ -0,0 +1,12 @@
+'''
+Configuration Settings
+
+Includes keys for Twilio, etc.
+'''
+
+# Uncommet to configure in file.
+ACCOUNT_SID = "AC8****************************" 
+AUTH_TOKEN = "f1****************************"
+MY_APP_SID = "PN5f****************************"
+MY_CALLER_ID = "+1415*****"
+WG_API_KEY = "ea********"
