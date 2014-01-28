b1c644f0ad75a88f5199fefa2fa5fd8bcbf08283
Phong Le <a.phong.le@gmail.com>
2014-01-27 23:39:58 -0800

    Barebone get foursquare data implementation.

--- weather_app/views.py
+++ weather_app/views.py
@@ -1,9 +1,18 @@
 from flask import (request, render_template, flash, redirect, g, url_for, session)
 from flask.ext.login import (login_user, logout_user, current_user,
     login_required)
+from flask.json import jsonify
+from ConfigParser import SafeConfigParser
+
+from foursquare import Foursquare
 
 from weather_app import app, db, login_manager
-from models import User
+from .models import User
+
+client_id = 'AYYIMF3RMVYQWYL0KSU1PES5QT4AKLXZUAUJTVISFCN1IABX'
+client_secret = '1G2NM12W44SNVTJ0RCKJ1WNOB0CVENAIQUZX0ZEXRPKQL1DO'
+redirect_uri = 'http://localhost:5000/callback'
+token_url = 'https://foursquare.com/oauth2/access_token'
 
 @login_manager.user_loader
 def load_user(id):
@@ -51,12 +60,12 @@ def login():
 def signup():
     error = None
     page = 'signup'
+
     if request.method == 'POST':
         user_check = User.query.filter_by(name=request.form['username']).first()
         if user_check != None:
             error = 'User name already taken, please choose another one'
         else:
-            session['logged_in'] = True
             user = User(name=request.form['username'],
                 password=request.form['password'], email=request.form['email'])
             db.session.add(user)
@@ -69,7 +78,6 @@ def signup():
 @app.route("/logout")
 @login_required
 def logout():
-    session.pop('logged_in', None)
     logout_user()
     return redirect(url_for('index'))
 
@@ -77,9 +85,50 @@ def logout():
 @login_required
 def forecast():
     # get hold of current user
+    user = current_user
 
-    # check if user has token
+    if user:
+        if user.fs_access_token is not None and user.fs_access_token != '':
+            # use the access token to get user's data
 
-    # if user has token then get the last check-in from foursquare
+            foursquare_client = Foursquare(access_token=user.fs_access_token)
+            user = foursquare_client.users()
+            return jsonify(user)
+        else:
+            # get secret and urls
+            #parser = SafeConfigParser()
+            #parser.read('foursquare.ini')
+            #client_id = parser.get('foursquare', 'client_id')
+            #client_secret = parser.get('foursquare', 'client_secret')
+            #redirect_uri = parser.get('foursquare', 'redirect_uri')
 
-    pass
+            # redirect user to foursquare authenticate page
+            foursquare_client = Foursquare(client_id=client_id,
+                client_secret=client_secret, redirect_uri=redirect_uri)
+            authorization_url = foursquare_client.oauth.auth_url()
+            return redirect(authorization_url)
+    else:
+        # no user (shouldn't happen)
+        return redirect(url_for('login'))
+
+@app.route('/callback', methods=['GET', ])
+def callback():
+    # get the current user
+    user = current_user
+
+    # parse out code from call back
+    code = request.args.get('code')
+    session['code'] = code
+
+    # exchange code for access token
+    foursquare_client = Foursquare(client_id=client_id,
+        client_secret=client_secret, redirect_uri=redirect_uri)
+    access_token = foursquare_client.oauth.get_token(code)
+
+    # save the access token
+    if access_token and access_token != '':
+        user.fs_access_token = access_token
+        db.session.commit()
+
+    return redirect(url_for('forecast'))
+
