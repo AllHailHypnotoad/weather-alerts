e2a2e42310365b5671642f7fedd73448f03b52b6
AButton <abutton@gmail.com>
2014-01-28 16:47:42 -0800

    updated WUnder to beable to use lat long info

--- WUnder.py
+++ WUnder.py
@@ -5,8 +5,8 @@ import local_settings
 WG_API_KEY = local_settings.WG_API_KEY
 
 
-def weather_forecast(zipcode):
-    search_url = "http://api.wunderground.com/api/%s/hourly/q/%s.json" % (WG_API_KEY, zipcode)
+def weather_forecast(lat, long):
+    search_url = "http://api.wunderground.com/api/%s/hourly/q/%s,%s.json" % (WG_API_KEY, lat, long)
     forecast_list = []
     fp = requests.get(search_url)
     forecasts = fp.json()
@@ -21,4 +21,6 @@ def weather_forecast(zipcode):
     return forecast_list
 
 if __name__ == '__main__':
-    print weather_forecast(94127)
+    lat1 = "37.73539734"
+    long1 = "-122.45811462"
+    print weather_forecast(lat1, long1)
