6220c24915c3791b20bebca08d5ec6326ec3e215
payoung <paul.andy.young@gmail.com>
2014-01-28 21:20:40 -0800

    got test file to work with new db config

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
-from database import init_db, db_session
-from models import *
+from weather_app import db
+from weather_app.models import *
 import datetime
 import unittest
 
@@ -11,22 +11,21 @@ class TestCase(unittest.TestCase):
             Need to modify the setup so that it points to  a testing database rather
             then the dev database
             '''
-            init_db()
+            db.create_all()
 
         def test_input_data(self):
-            print db_session.bind
             u1 = User(name="Paul", email="paul.andy.young@gmail.com", 
-                password="password")
-            db_session.add(u1)
-            db_session.commit()
+                password="password", fs_access_token='asdfasdfasdwegdfg')
+            db.session.add(u1)
+            db.session.commit()
             print "*****Data has been committed to DB*****"
         
         def test_query1(self):
-            user1 = db_session.query(User).filter_by(name='Paul').first()
+            user1 = User.query.filter_by(name='Paul').first()
             print "User Name: ", user1.name, "User Email: ", user1.email
 
         def tearDown(self):
-            db_session.remove()
+            db.session.remove()
 
 
 
